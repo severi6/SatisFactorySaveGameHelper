a00c5b88bd21a8e85a418386335caa5f62973f8d
Severi 6 <162328226+severi6@users.noreply.github.com>
2024-10-13 13:03:12 +0300

    Update SatisFactorySaveDumper.bat

--- SatisFactorySaveDumper.bat
+++ SatisFactorySaveDumper.bat
@@ -1,4 +1,4 @@
 @echo off
-set "BACKUP_DIR=%USERPROFILE%\Downloads\Satisfactory_SaveGames" :: checks for a folder in downloads
-if not exist "%BACKUP_DIR%" mkdir "%BACKUP_DIR%" :: creates a folder in downloads
-xcopy /S /I "%USERPROFILE%\AppData\Local\FactoryGame\Saved\SaveGames" "%BACKUP_DIR%" :: copies files from factorygame folder to downloads folder created on line 2
+set "BACKUP_DIR=%USERPROFILE%\Downloads\Satisfactory_SaveGames"
+if not exist "%BACKUP_DIR%" mkdir "%BACKUP_DIR%"
+xcopy /S /I "%USERPROFILE%\AppData\Local\FactoryGame\Saved\SaveGames" "%BACKUP_DIR%"
